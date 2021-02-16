46623758ad74a65423f67fb9477593923367f1cd
ayham <ayhamsysy@gmail.com>
2021-02-16 13:20:17 +0800

    add comment to all new Devs

--- src/constants.js
+++ src/constants.js
@@ -8,4 +8,8 @@ const dev = {
   paymentUrl: "https://www.billplz-sandbox.com",
 };
 
+
+//Devs should add this file to .gitIgnore and modify it as they wish
+//Devs do not commit this file if you change it
+//TASK-TO-WAN Please revert this line and commit it, 
 export const config = process.env.NODE_ENV === "development" ? prod : prod;
